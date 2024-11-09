f44c7b46e99f90a602a2b4b3c973500bc0a8d370
csm031231 <csumim@naver.com>
2024-11-10 00:07:26 +0900

    feat : 검색 결과 페이지 작성

--- Front/inje-pgv/src/routes/RootRoute.jsx
+++ Front/inje-pgv/src/routes/RootRoute.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { MovieMainPage } from "../pages/MovieMainPage";
 import { Mainpage,  } from "../pages/MainPage";
 import { Signup } from "../pages/signup";
+import { SearchPage } from "../component/Search";
 
 export const RootRoutes = () => {
   return (
@@ -9,6 +10,7 @@ export const RootRoutes = () => {
       <Route path="/" element={<Mainpage />} />
       <Route path="/Signup" element={<Signup />} />
       <Route path="/MovieMainPage" element={<MovieMainPage />} />
+      <Route path="/SearchPage" element={<SearchPage />} />
     </Routes>
   );
 };
