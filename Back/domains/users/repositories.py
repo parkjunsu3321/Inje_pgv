490a4ce327e97ce57fae28c32348ae7523d0042a
Leehyukjun <mdt0318@naver.com>
2024-11-09 19:04:47 +0900

    feat : 로그인 회원가입 구현

--- Back/domains/users/repositories.py
+++ Back/domains/users/repositories.py
@@ -2,8 +2,26 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException, status
 from .models import UserModel
+from dependencies.auth import hash_password
+from domains.users.dto import User
 
 
 class UserRepository:
     def __init__(self, session: Session):
         self._session = session
+
+    def find_id(self, user_id: str):
+        return self.db.query(UserModel).filter(UserModel.id == user_id).first()
+
+    def add_user(self, user_data: User):
+        db_user = UserModel(
+            id=user_data.id,
+            name=user_data.name,
+            nickname=user_data.nickname,
+            password=hash_password(user_data.password),
+            birth_date=user_data.birth_date
+        )
+        self.db.add(db_user)
+        self.db.commit()
+        self.db.refresh(db_user)
+        return db_user
--- Back/routers/users/user_controller.py
+++ Back/routers/users/user_controller.py
@@ -4,6 +4,7 @@ from dependencies.database import provide_session, Session
 from domains.users.dto import LoginUser, User
 from domains.users.models import UserModel
 from dependencies.auth import verify_password,create_access_token, hash_password
+from domains.users.services import UserService
 
 
 name = "users"
@@ -11,31 +12,13 @@ router = APIRouter()
 
 @router.post("/login")
 def login_user(lg_user: LoginUser, db: Session = Depends(provide_session)):
-    user = db.query(UserModel).filter(UserModel.id == lg_user.id).first()
-    if user:
-        if verify_password(lg_user.password, user.password) :
-            s="로그인 성공"
-            token = create_access_token({"sub": "id"})
-            return s, token
-        else:
-            s="비밀번호 불일치"
-    else:
-        s="일치하는 학번 없음"
-    
-    return s
+    result, token = UserService.login_service(lg_user, db)
+    return {"message": result, "token": token}
+
     
 
 @router.post("/register", response_model=User)
 def register_user(user: User, db: Session = Depends(provide_session)):
-    # 회원가입 로직
-    db_user = UserModel(
-        id=user.id,
-        name=user.name,
-        nickname=user.nickname,
-        password=hash_password(user.password),
-        birth_date=user.birth_date
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    user_service = UserService(db)  # UserService 인스턴스 생성
+    db_user = user_service.register_user(user)  # 서비스 계층에서 회원가입 로직 처리
+    return db_user  # 클라이언트에 결과 반환
